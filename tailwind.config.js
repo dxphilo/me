f9930fd5ee4a055d974eb7935f19aebd84a44937
John Philip <developerphilo@gmail.com>
2021-10-09 05:16:05 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
+  darkMode: 'media',
   purge: [
     './components/**/*.{vue,js}',
     './layouts/**/*.vue',
