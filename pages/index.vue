a73f1d329ba39c807fa2b39708e7d81b5d1fd5d3
John Philip <developerphilo@gmail.com>
2022-03-23 23:19:51 +0300

    feat: added the infro component

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,17 @@
   <!-- component -->
   <div>
     <Intro />
+    <Info />
   </div>
 </template>
 <script>
 import Intro from '@/components/Intro.vue'
+import Info from '@/components/Info.vue'
 export default {
 	components: {
 		Intro,
+		Info,
 	},
 }
 </script>
-<
 <style scoped></style>
