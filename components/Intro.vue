de6d74cbdbc0b0ca971527345cc674956947aa0d
John Philip <developerphilo@gmail.com>
2022-03-23 23:19:51 +0300

    feat: added the infro component

--- components/Intro.vue
+++ components/Intro.vue
@@ -21,6 +21,7 @@ export default {}
 <style scoped>
 .headerr {
 	height: 450px;
-	background-image:linear-gradient(rgba(0, 0, 0, 0.518), rgba(0, 0, 0, 0.518)),url('https://images.pexels.com/photos/8775535/pexels-photo-8775535.jpeg?auto=compress&cs=tinysrgb&fit=crop&fp-y=0.5&h=500&sharp=10&w=2000');
+	background-image: linear-gradient(rgba(0, 0, 0, 0.518), rgba(0, 0, 0, 0.518)),
+		url('https://images.pexels.com/photos/8775535/pexels-photo-8775535.jpeg?auto=compress&cs=tinysrgb&fit=crop&fp-y=0.5&h=500&sharp=10&w=2000');
 }
 </style>
