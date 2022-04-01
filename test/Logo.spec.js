6384cac18b29a8653d583898eae1f328a9dc535f
John Philip <developerphilo@gmail.com>
2022-04-01 23:41:43 +0300

    fix: reset repo

--- test/Logo.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Header from '@/components/Header.vue'
-
-describe('Header', () => {
-	test('is a Vue instance', () => {
-		const wrapper = mount(Header)
-		expect(wrapper.vm).toBeTruthy()
-	})
-})
