783c4091020411e2a1425febcc1619f03486c666
John Philip <developerphilo@gmail.com>
2022-07-08 19:33:39 +0300

    fix: lint error

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,7 +1,12 @@
 <template>
   <!-- component -->
   <div>
-    <div class="w-full h-96 bg-no-repeat object-center bg-center bg-cover" :style="{'background-image': `url(https://images.pexels.com/photos/924824/pexels-photo-924824.jpeg?auto=compress&cs=tinysrgb&w=1600)`}">
+    <div
+      class="w-full h-96 bg-no-repeat object-center bg-center bg-cover"
+      :style="{
+        'background-image': `url(https://images.pexels.com/photos/924824/pexels-photo-924824.jpeg?auto=compress&cs=tinysrgb&w=1600)`,
+      }"
+    >
       <div class="flex items-center justify-center">
         <div class="p-32 text-gray-300">
           <h1 class="text-center font-bold text-3xl">
@@ -18,5 +23,4 @@ export default {
   name: 'Intro',
 }
 </script>
-<style scoped>
-</style>
+<style scoped></style>
--- content/articles/make-money-with-technical-writing.md
+++ content/articles/make-money-with-technical-writing.md
@@ -7,7 +7,6 @@ alt: 5 ways to make money with technical writing
 
 excerpt: 5 ways to make money with technical writing
 
-
 imgurl: https://images.pexels.com/photos/534229/pexels-photo-534229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
 
 tag: WRITING
